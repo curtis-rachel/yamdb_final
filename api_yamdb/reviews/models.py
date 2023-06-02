74d3e7e8040e24f38f6a2b17b4ec720f63f5928b
Рачеев Артём <curtis1rachel@yandex.ru>
2023-06-02 19:51:27 +0300

    2

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,8 +1,8 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import ASCIIUsernameValidator
 from django.core.validators import (
     MinLengthValidator,
     MaxValueValidator)
-from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.validators import ASCIIUsernameValidator
 from django.db import models
 from django.db.models import TextField
 
